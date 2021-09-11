b876b416fcc8b0321e83793c7e1952fd3a84ef49
Екатерина <81039066+katerina-leo@users.noreply.github.com>
2021-09-11 22:44:49 +0300

    Завершение проекта
    Стилизация проекта, создана сетка,

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const channelSlider = new Swiper('.popular-slider', {
+  // Optional parameters
+  
+  loop: true,
+  
+  // Navigation arrows
+  navigation: {
+    nextEl: '.button-next',
+    prevEl: '.button-prev',
+  }
+});
